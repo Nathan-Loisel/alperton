23dcc6fddaacad2907c6ea3a9bd1cd2673f078be
IronHoldMC <129500516+IronHoldMC@users.noreply.github.com>
2023-10-12 12:43:27 +0200

    Update path.py

--- modules/path.py
+++ modules/path.py
@@ -1,11 +1,11 @@
-from alperton import COLORS, STATUS
+#from alperton import COLORS, STATUS
 
 def run():
-    print( STATUS.info + " Checking $PATH")
+    #print( STATUS.info + " Checking $PATH")
     path_command = os.environ["PATH"]
     path_command = path_command.split(":")
     for i in path_command:
         if i == ".":
-            print(STATUS.good + " Path has a '.' entry")
+    #        print(STATUS.good + " Path has a '.' entry")
             break
     print("")
